faf0bb705dcd446a00d0601d5c020bcfee47023d
madrigal1 <kurupgokul11@gmail.com>
2020-06-13 14:53:17 +0530

    depploy: heroku v1.5

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,20 @@
 export const port = process.env.PORT;
 export const environment = process.env.NODE_ENV;
 
+interface db {
+    name: string;
+    host: string;
+    port: string;
+    user: string;
+    password: string;
+}
 
-export const db = {
-    name: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_USER_PWD,
+export const db: db = {
+    name: process.env.DB_NAME as string,
+    host: process.env.DB_HOST as string,
+    port: process.env.DB_PORT as string,
+    user: process.env.DB_USER as string,
+    password: process.env.DB_USER_PWD as string,
 }
 
 
