e57c8da1b1245a05e686a73ca42296533c41ac06
madrigal1 <kurupgokul11@gmail.com>
2020-06-13 17:22:50 +0530

    deploy:v3.1 heroku

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import app from "./app";
 import { port, host } from "./config";
 import Logger from "./core/Logger";
 
+Logger.info('debug' + port);
 
 app
-    .listen(parseInt(port), host, () => {
+    .listen(port || 3000, () => {
         Logger.info(`Server running on : ${port}`);
     })
     .on('error', (e) => Logger.error(e));
